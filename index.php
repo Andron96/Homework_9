834c7461517ac21f23df9e64ff284ecc5e264630
Andron96 <drandarik2@gmail.com>
2023-12-31 16:47:47 +0200

    3

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 class tasksHandler
 {
-    public $tasksList, $taskName, $taskPriority;
-    private $taskId;
+    public array $tasksList;
+    public string $taskName;
+    public int $taskPriority;
+    private int $taskId;
+    function __construct(array &$tasksList)
+    {
+        $this->tasksList = &$tasksList;
+    }
+    public function addTask(string $taskName, int $taskPriority)
+    {
+        $this->tasksList[] = ['Task name' => $taskName, 'Task priority' => $taskPriority];
+    }
 }
 
 $tasksList = [];
+$tasksHandler = new tasksHandler($tasksList);
+$tasksHandler->addTask("new task", 1);
+var_dump($tasksList);
