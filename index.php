9d65d89b66e3c96e8038d19acc22f969adb90eb0
Andron96 <drandarik2@gmail.com>
2024-01-02 19:42:57 +0200

    4

--- index.php
+++ index.php
@@ -2,20 +2,52 @@
 class tasksHandler
 {
     public array $tasksList;
-    public string $taskName;
-    public int $taskPriority;
-    private int $taskId;
     function __construct(array &$tasksList)
     {
         $this->tasksList = &$tasksList;
     }
-    public function addTask(string $taskName, int $taskPriority)
+    public function addTask(string $taskName, int $taskPriority): string
     {
-        $this->tasksList[] = ['Task name' => $taskName, 'Task priority' => $taskPriority];
+        if (($taskPriority >= 1) && ($taskPriority <= 15)) {
+            if (!$this->tasksList) {
+                $taskId = 0;
+            } else {
+                $taskId = array_key_last($this->tasksList) + 1;
+            }
+            $this->tasksList[] = ['Task ID' => $taskId, 'Task priority' => $taskPriority, 'Task name' => $taskName];
+            echo "Задача добавлена.\n";
+        } else {
+            echo "Задача не добавлена из-за неправильного значения приоритета.\n";
+        }
+        if (!$this->tasksList) {
+            return "*****\nСписок задач пуст.\n*****\n";
+        } else {
+            echo "*****\nНачало списка задач.\n";
+            foreach ($this->tasksList as $item) {
+                echo "ID задачи: " . $item['Task ID'] . "; Приоритет задачи: " . $item['Task priority'] . "; Имя задачи: " . $item['Task name'] . "." . "\n";
+            }
+            return "Конец списка задач.\n*****\n";
+        }
+    }
+    public function deleteTask(int $taskId)
+    {
+
     }
 }
 
 $tasksList = [];
 $tasksHandler = new tasksHandler($tasksList);
-$tasksHandler->addTask("new task", 1);
-var_dump($tasksList);
+while (true) {
+    echo "Введите \"1\", что бы добавать задачу\n";
+    echo "Введите \"2\", что бы удалить задачу\n";
+    $select = (int) readline("Ввод чего-либо другого прекратит выполнение программы: ");
+    switch ($select) {
+        case 1:
+            $taskPriority = (int) readline("Введите приоритет задачи (от 1 до 15): ");
+            $taskName = (string) readline("Введите имя задачи: ");
+            echo $tasksHandler->addTask($taskName, $taskPriority);
+            break;
+        default:
+            break 2;
+    }
+}
